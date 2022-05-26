62adfb32ac9fc7368932c4dcb7ad230926e11ea4
Gabriel Barreto <gabriel-gdr@hotmail.com>
2022-05-25 21:55:42 -0300

    refactor: Substituida função que inicializa a aplicação por uma IIFE

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,11 @@ import { createSlidersEvents } from './controllers.js';
 
 document.addEventListener('DOMContentLoaded', createSlidersEvents);
 
+/**
+ * Quantidade de pokémons existentes
+ */
+const POKEMON_COUNT = 898;
+
 const modal = document.querySelector('.controller-modal');
 
 document.querySelector('#open-modal').addEventListener('click', () => {
@@ -14,15 +19,12 @@ document.querySelector('#close-modal').addEventListener('click', () => {
   modal.style.display = 'none';
 });
 
-const getPokemonList = () => {
+/**
+ * IIFE que inicializa a aplicação
+ */
+(function () {
   for (let i = 0; i < 25; i++) {
-    const pokemonId = Math.ceil(Math.random() * 898);
+    const pokemonId = Math.ceil(Math.random() * POKEMON_COUNT);
     createPokemonCard(`${API_URL}/pokemon/${pokemonId}`);
   }
-};
-
-getPokemonList();
-
-function teste() {
-  console.log('teste');
-}
+})();
